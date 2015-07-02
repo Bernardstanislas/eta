daca9416ea7a8782b766f14efd93b6733dcd410c
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-02 23:10:45 +0200

    [hotfix] [header] Fix path to background image

--- site/src/views/header/index.js
+++ site/src/views/header/index.js
@@ -7,7 +7,7 @@ let Header = React.createClass({
             padding: '0'
         },
         background: {
-            backgroundImage: 'url("../images/header.jpg")',
+            backgroundImage: 'url("./images/header.jpg")',
             backgroundSize: 'cover',
             backgroundPositionY: '20%',
             height: '200px',
