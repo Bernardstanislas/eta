9f0f9e6acebb3598914c20c9911b2908b3448abb
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-02 23:23:54 +0200

    [projects] Add a dummy page

--- site/src/views/projects/index.js
+++ site/src/views/projects/index.js
@@ -1,9 +1,36 @@
 let Projects = React.createClass({
     displayName: 'Projects',
+    style: {
+        title: {
+            fontSize: '2em'
+        },
+        gallery: {
+            display: 'flex',
+            flexWrap: 'wrap'
+        },
+        thumbnail: {
+            width: '200px',
+            margin: '5px'
+        }
+    },
     render() {
+        let fakeArray = [];
+        for (let i = 0; i < 50; i++) {
+            fakeArray[i] = 'dummy';
+        }
+        let {style} = this;
         return (
             <div>
-
+                <h1 style={style.title}>Projets réalisés</h1>
+                <div style={style.gallery}>
+                    {fakeArray.map(() => {
+                        return (
+                            <div style={style.thumbnail}>
+                                <img src='http://dummyimage.com/200x150/6e6e6e/ababab&text=Projet' style={style.image}/>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
