0271446819749f50f25937c7e9751a2636a9cf5e
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-02 23:05:59 +0200

    Remove old code

--- site/src/index.js
+++ site/src/index.js
@@ -1,8 +1,6 @@
 let Introduction = require('views/introduction');
-//let App = require('views/app');
 
 React.render(React.createElement(Introduction), window.document.getElementById('page'));
 setTimeout(() => {
-    //React.render(React.createElement(App), window.document.getElementById('page'));
     require('views/app');
 }, 3000);
